d291daac77a19776f252f7bc382b1b3aaf2e4607
ifadgame0-glitch <ifadgame0@gmail.com>
2026-05-17 13:03:26 +0600

    Update bot leave message format and author

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,30 +1,43 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
-module.exports = {
-config: {
-name: "Out",
-aliases: ["l"],
-version: "1.0",
-author: "Sandy",
-countDown: 5,
-role: 2,
-shortDescription: "bot will leave gc",
-longDescription: "",
-category: "admin",
-guide: {
-vi: "{pn} [tid,blank]",
-en: "{pn} [tid,blank]"
-}
-},
 
-onStart: async function ({ api,event,args, message }) {
-var id;
-if (!args.join(" ")) {
-id = event.threadID;
-} else {
-id = parseInt(args.join(" "));
-}
-return api.sendMessage('▣𝗔𝗬𝗔𝗡 𝗕𝗢𝗧 𝗟𝗘𝗔𝗩𝗘:\n》Ami toder sukh dewar jonno Ashchilam tora etar joggo na.\n\n➤𝗕𝗘𝗬 𝗟𝗘𝗦 𝗡𝗔𝗭𝗘𝗦', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-}
+module.exports = {
+	config: {
+		name: "Out",
+		aliases: ["l"],
+		version: "1.0",
+		author: "Efuu",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot will leave gc",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api, event, args, message }) {
+		var id;
+
+		if (!args.join(" ")) {
+			id = event.threadID;
+		} else {
+			id = parseInt(args.join(" "));
+		}
+
+		return api.sendMessage(
+			'╔═════ஜ۩۞۩ஜ═════╗\n' +
+			'         『 𝗘𝗙𝗨𝗨 𝗕𝗢𝗧 』\n' +
+			'╚═════ஜ۩۞۩ஜ═════╝\n\n' +
+			'➤ Bot is leaving this group...\n' +
+			'➤ Thanks for using EFUU BOT 💖\n\n' +
+			'━━━━━━━━━━━━━━━━━━\n' +
+			'      𝗕𝗬𝗘 𝗘𝗩𝗘𝗥𝗬𝗢𝗡𝗘 👋',
+			id,
+			() => api.removeUserFromGroup(api.getCurrentUserID(), id)
+		);
+	}
 }
